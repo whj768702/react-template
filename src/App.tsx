31fff7fb44348bb6b8f39ba14dcf3105a6da325f
whj768702 <whj768702@gmail.com>
2023-05-24 15:43:31 +0800

    feat(init): init template project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-900">
+        Hello world!
+      </h1>
+      <Button type="primary">Button</Button>
+    </>
+  )
+}
+
+export default App
